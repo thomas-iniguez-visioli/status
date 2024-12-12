9872a8270d83b4c5c552e1bb698762ec58a7aace
arbinger <thomas.iniguez+github@free.fr>
2024-12-12 19:01:39 +0100

    autosetup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const fs =require("fs")
+const fs = require("fs");
+const YAML = require('yaml')
+const upptimeConfig = YAML.parse(fs.readFileSync("./.upptimerc.yml").toString());
+const tempFiles = fs.readdirSync("./temp").filter(file => file.endsWith('.json'));
+console.log(upptimeConfig)
+tempFiles.forEach(file => {
+  const tempData = require(`./temp/${file}`);
+  upptimeConfig.sites.push({
+    name: tempData.name,
+    url: tempData.url
+  });
+  fs.rmSync(`./temp/${file}`)
+});
+
+fs.writeFileSync("./.upptimerc.yml", YAML.stringify(upptimeConfig));
